f558d12b8e8bc3dfe05a7c05773b370e6ca3fc0d
Mikah Mathews <mikah@outofband.org>
2020-05-08 09:39:56 -0700

    Add bakerycart

--- bakery.cs
+++ bakery.cs
@@ -7,6 +7,23 @@ namespace BakeryOptions
   {
     public string Bread { get; set;}
     public string Pastry { get; set;}
-    
+    public int Price {get; set;}
+
+    public static Dictionary<string, int> BakeStats;
+
+    public BakeryCart(string numberOfBread, string numberOfPastry, int total)
+    {
+      Bread = numberOfBread;
+      Pastry = numberOfPastry;
+      Price = total;
+      BakeStats = new Dictionary<string, int>();
+      BakeStats.Add("Bread", 0);
+      BakeStats.Add("Pastry", 0);
+    }
+
+    public static string addBread
+    {
+      
+    }
   }
 }
