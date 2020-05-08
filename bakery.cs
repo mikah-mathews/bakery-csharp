7be158c1076ee0d08ee18a8cc3b6216f93538856
Mikah Mathews <mikah@outofband.org>
2020-05-08 09:10:40 -0700

    add start of public classes

---
+++ bakery.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace BakeryOptions
+{
+  public class Carbs
+  {
+    public string Bread { get; set;}
+    public string Pastry { get; set;}
+    
+  }
+}
